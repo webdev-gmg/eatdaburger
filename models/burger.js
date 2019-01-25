7d96befe8bc1294b71477c22509a66168c4ef76b
unknown <godly.mg2@gmail.com>
2019-01-24 22:48:30 -0500

    heroku

--- models/burger.js
+++ models/burger.js
@@ -2,22 +2,22 @@ const orm = require("../config/orm.js");
 
 const burger = {
   all: function (cb) {
-    orm.all("burger", res => {
+    orm.all("burgers", res => {
       cb(res);
     });
   },
   create: function (cols, vals, cb) {
-    orm.create("burger", cols, vals, res => {
+    orm.create("burgers", cols, vals, res => {
       cb(res);
     });
   },
   update: function (objColVals, condition, cb) {
-    orm.update("burger", objColVals, condition, res => {
+    orm.update("burgers", objColVals, condition, res => {
       cb(res);
     });
   },
   delete: function (condition, cb) {
-    orm.delete("burger", condition, res => {
+    orm.delete("burgers", condition, res => {
       cb(res);
     });
   }
