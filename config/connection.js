411732e544f911458004539f1ac4e87e4b9f2f2a
unknown <godly.mg2@gmail.com>
2019-01-26 08:59:03 -0500

    Test Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,29 @@
-const mysql = require("mysql");
+// dependencies
+var mysql = require("mysql");
+var connection;
 
-
-let connection;
-
+// uses JawsDB to configure connection
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        port: 3306,
-        database: 'burgers_db'
-    })
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
 }
 
-connection.connect(err => {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+// make connection
+connection.connect(function(err) {
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id: " + connection.threadId);
 });
 
+// export connection for ORM
 module.exports = connection;
--- models/burger.js
+++ models/burger.js
@@ -2,22 +2,22 @@ const orm = require("../config/orm.js");
 
 const burger = {
   all: function (cb) {
-    orm.all("burgers", res => {
+    orm.all("BURGERS", res => {
       cb(res);
     });
   },
   create: function (cols, vals, cb) {
-    orm.create("burgers", cols, vals, res => {
+    orm.create("BURGERS", cols, vals, res => {
       cb(res);
     });
   },
   update: function (objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, res => {
+    orm.update("BURGERS", objColVals, condition, res => {
       cb(res);
     });
   },
   delete: function (condition, cb) {
-    orm.delete("burgers", condition, res => {
+    orm.delete("BURGERS", condition, res => {
       cb(res);
     });
   }
