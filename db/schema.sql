5e2e7666f33201581bfd0669ddec9e1aca74d808
unknown <godly.mg2@gmail.com>
2019-01-26 09:03:29 -0500

    Test Heroku

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers (
+CREATE TABLE BURGERS (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  devoured BOOLEAN,
+  name VARCHAR(100) NOT NULL,
+  devoured BOOLEAN, 
   PRIMARY KEY (id)
 );
